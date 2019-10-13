968475c28b75eb6e12da08ac42c4a2037b0139f1
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-10-13 19:24:04 -0400

    added service layer and moved service implementations

---
+++ AttendanceLite.Services/TimeLogService.cs
@@ -0,0 +1,40 @@
+﻿using AttendanceLite.Domain.Entities;
+using AttendanceLite.Domain.Exceptions;
+using AttendanceLite.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttendanceLite.Services
+{
+    public class TimeLogService
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly ITimeLogRepository _timeLogRepo;
+
+        public TimeLogService(IUserRepository userRepository, ITimeLogRepository timeLogRepository)
+        {
+            _userRepo = userRepository;
+            _timeLogRepo = timeLogRepository;
+        }
+        public void AddLog(TimeLog timeLog)
+        {
+            if (timeLog == null)
+                throw new TimeLogNullException(nameof(AddLog));
+
+            var user = _userRepo.GetBy(timeLog.UserId);
+
+            if (user == null)
+                throw new UserNullException(nameof(AddLog));
+
+            _timeLogRepo.Add(timeLog);
+            user.AddLog(timeLog);
+
+        }
+
+        public void DeleteLog(int id)
+        {
+            _timeLogRepo.Remove(id);
+        }
+    }
+}
